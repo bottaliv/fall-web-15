6316391fae229834f69db9367da64656b4622464
bottaliv <vbottali7@gmail.com>
2015-09-03 14:44:34 +0000

    bootstrap

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+
+  </head>
+  <body>
+      <div class="container">
+          <div class="row">
+              <div class="col-sm-8">
+                  
+              </div>
+              <div class"col-sm-4"></div>
+          </div>
+    <h1>Hello, world!</h1>
+    <a href="#" class="btn btn-success">
+    <i class="glyphicon glyphicon-plus"></i>    
+        New Record
+        </a>
+    <a href="#" class="btn btn-danger">Delete All
+    <i class="glyphicon glyphicon-trash"></i>
+    </a>
+    
+    <!-- Indicates a successful or positive action -->
+<button type="button" class="btn btn-success">Success</button>
+
+<!-- Contextual button for informational alert messages -->
+<button type="button" class="btn btn-info">Info</button>
+
+<!-- Indicates caution should be taken with this action -->
+<button type="button" class="btn btn-warning">Warning</button>
+
+   <div class="progress">
+  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+    <span class="sr-only">45% Complete</span>
+  </div>
+</div>
+    
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- animation script -->
+    <script type="text/javascript" 
+ 
+  </body>
+</html>
